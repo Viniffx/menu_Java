70de7e24a9d74c302cb6cd8008b47c3fa6436bf8
viniffx <viniffx@gmail.com>
2026-03-17 09:19:14 -0300

    17-03-26

--- menuObjeto/src/Leitura.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Leitura {
-    public static String dados(String mensagem){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(mensagem);
-        return scanner.nextLine();
-    }
-}
--- menuObjeto/src/Periodo.java
+++
@@ -1,6 +0,0 @@
-public enum Periodo {
-    MATUTINO,
-    VESPERTINO,
-    NOTURNO,
-    INTEGRAL
-}
